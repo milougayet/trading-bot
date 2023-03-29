572d49d615b1eed511fd17958a237fb8144f4882
Loua <70914829+milougayet@users.noreply.github.com>
2023-03-29 19:50:29 +0200

    Update MeasuredFluorescentPinkCow.py

--- MeasuredFluorescentPinkCow.py
+++ MeasuredFluorescentPinkCow.py
@@ -1,4 +1,7 @@
+#region imports
 from AlgorithmImports import *
+
+#endregion
 
 class MeasuredFluorescentPinkCow(QCAlgorithm):
 
@@ -10,16 +13,33 @@ class MeasuredFluorescentPinkCow(QCAlgorithm):
         spy = self.AddEquity("SPY", Resolution.Daily)
         # self.AddForex, self.AddFuture...
         
-        spy.SetDataNormalizationMode(DataNormalization.Raw)
+        spy.SetDataNormalizationMode(DataNormalizationMode.Raw)
 
         self.spy = spy.Symbol
 
         self.SetBenchmark("SPY")
-        self.SetBrokarageModel(BrokerageName.InteractiveBrokersBrokarage, AccountType.Margin)
+        self.SetBrokerageModel(BrokerageName.InteractiveBrokersBrokerage, AccountType.Margin)
 
         self.entryPrice = 0
         self.period = timedelta(31)
         self.nextEntryTime = self.Time
 
     def OnData(self, data): # every time a tick event occurs or a bar reaches its end time
-        pass
+        if not self.spy in data:
+            return
+        
+        # price = data.Bars[self.spy].Close
+        price = data[self.spy].Close
+        # price = self.Secutiries[self.spy].Close
+
+        if not self.Portfolio.Invested:
+            if self.nextEntryTime <= self.Time:
+                self.SetHoldings(self.spy, 1)
+                # self.MarketOrder(self.spy, int(self.Portfolio.Cash / price ))
+                self.Log("BUY SPY @" + str(price))
+                self.entryPrice = price
+
+        elif self.entryPrice * 1.1 < price or self.entryPrice * 0.9 > price:
+            self.Liquidate()
+            self.Log("SELL SPY @" + str(price))
+            self.nextEntryTime = self.Time + self.period
